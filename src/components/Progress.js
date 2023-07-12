02d81eeaa06588770d6fc13a102ca73c8eb60a19
KarinaChumak <karinac3011@gmail.com>
2023-07-12 10:41:07 +0200

    Initial commit

---
+++ src/components/Progress.js
@@ -0,0 +1,26 @@
+function Progress({
+  index,
+  numQuestions,
+  points,
+  totalPoints,
+  answer,
+}) {
+  return (
+    <header className="progress">
+      <progress
+        max={numQuestions}
+        value={index + Number(answer !== null)}
+      ></progress>
+      <p>
+        {' '}
+        Question = <strong>{index + 1}</strong> / {numQuestions}
+      </p>
+      <p>
+        {' '}
+        {points}/{totalPoints}
+      </p>
+    </header>
+  );
+}
+
+export default Progress;
